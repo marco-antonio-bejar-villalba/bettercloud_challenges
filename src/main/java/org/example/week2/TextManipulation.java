7a994fcfb0105967000356448cb3ab708ee5cc85
Marco <marco.antonio.bejar.villalba@gmail.com>
2023-11-26 11:16:57 -0600

    Fix

--- src/main/java/org/example/week2/TextManipulation.java
+++ src/main/java/org/example/week2/TextManipulation.java
@@ -5,31 +5,31 @@ public class TextManipulation {
     public static void main(String[] args) {
         String telephone = "123-456-7890";
 
-        System.out.println(hidePhonea(telephone));
-        System.out.println(hidePhoneb(telephone));
-        System.out.println(hidePhonec(telephone));
-        System.out.println(hidePhoned(telephone));
+        System.out.println(hidePhoneA(telephone));
+        System.out.println(hidePhoneB(telephone));
+        System.out.println(hidePhoneC(telephone));
+        System.out.println(hidePhoneD(telephone));
 
 
     }
 
 
-    public static String hidePhonea(String fullPhoneNumber) {
+    public static String hidePhoneA(String fullPhoneNumber) {
 
         String mask = "xxx-xxx-";
        // mask.append(fullPhoneNumber.substring(8));
         return mask;
     }
 
-    public static String hidePhoneb(String fullPhoneNumber) {
+    public static String hidePhoneB(String fullPhoneNumber) {
         return new StringBuilder("xxx-xxx-") + fullPhoneNumber.substring(8);
     }
 
-    public static String hidePhonec(String fullPhoneNumber) {
+    public static String hidePhoneC(String fullPhoneNumber) {
         return new StringBuilder(fullPhoneNumber).replace(0, 7, "xxx-xxx-").toString();
     }
 
-    public static String hidePhoned(String fullPhoneNumber) {
+    public static String hidePhoneD(String fullPhoneNumber) {
         return "xxx-xxx-" + fullPhoneNumber.substring(8, 12);
     }
 
